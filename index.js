156fb4242c9dc6c2b5108e3715a3b95c13533c37
YohellBuzo <yohellbuzo7@gmail.com>
2025-11-13 16:37:06 -0600

    Mejoras

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import "dotenv/config";
 import cron from "node-cron";
 import { syncHistorical } from "./src/syncHistorical.js";
 import { log } from "./src/logger.js";
-import express from "express";
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 async function main() {
   log("Servidor iniciado 🟢");
@@ -18,11 +14,4 @@ async function main() {
   });
 }
 
-app.get("/", (req, res) => {
-  res.send("🟢 Lotería sync en ejecución");
-});
-
-app.listen(PORT, () => {
-  log(`Servidor escuchando en puerto ${PORT}`);
-  main();
-});
+main();
