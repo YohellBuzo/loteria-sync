56cf654238cfdf379ff5d4ce44cd35bacc6271fe
YohellBuzo <yohellbuzo7@gmail.com>
2025-11-13 20:06:32 -0600

    Mejoras

--- src/syncHistorical.js
+++ src/syncHistorical.js
@@ -3,49 +3,60 @@ import { getByDrawNumber } from "./api.js";
 import { log } from "./logger.js";
 
 export async function syncHistorical() {
-  const { rows } = await db.query("SELECT numero_sorteo FROM sincronizacion;");
+  try {
+    const { rows } = await db.query(
+      "SELECT numero_sorteo FROM sincronizacion;"
+    );
 
-  let last = rows[0].numero_sorteo;
-  let next = Number(last) + 1;
+    let last = rows[0].numero_sorteo;
+    let next = Number(last) + 1;
 
-  log("Buscando sorteo histórico:", next);
+    log("Buscando sorteo histórico:", next);
 
-  const sorteo = await getByDrawNumber(next);
-  sorteo.premios.sort((a, b) => Number(a.tipo) - Number(b.tipo));
+    const sorteo = await getByDrawNumber(next);
+    sorteo.premios.sort((a, b) => Number(a.tipo) - Number(b.tipo));
 
-  if (!sorteo) {
-    log("No existe el sorteo", next, "se detiene por hoy.");
-    return process.exit();
-  }
+    if (!sorteo) {
+      log("No existe el sorteo", next, "se detiene por hoy.");
+      return process.exit();
+    }
 
-  await db.query(
-    `INSERT INTO sorteos_loteria (numero_sorteo, fecha)
+    await db.query(
+      `INSERT INTO sorteos_loteria (numero_sorteo, fecha)
      VALUES ($1, $2)
      ON CONFLICT (id) DO NOTHING`,
-    [sorteo.numeroSorteo, sorteo.fecha]
-  );
+      [sorteo.numeroSorteo, sorteo.fecha]
+    );
 
-  const inserts = sorteo.premios.slice(0, 3).map((numero_ganador) => {
-    return db.query(
-      `INSERT INTO numeros_ganadores_loteria (numero, premio, serie, numero_sorteo)
+    const inserts = sorteo.premios.slice(0, 3).map((numero_ganador) => {
+      return db.query(
+        `INSERT INTO numeros_ganadores_loteria (numero, premio, serie, numero_sorteo)
      VALUES ($1, $2, $3, $4)
      ON CONFLICT (id) DO NOTHING`,
-      [
-        numero_ganador.numero,
-        numero_ganador.tipo,
-        numero_ganador.serie,
-        sorteo.numeroSorteo,
-      ]
+        [
+          numero_ganador.numero,
+          numero_ganador.tipo,
+          numero_ganador.serie,
+          sorteo.numeroSorteo,
+        ]
+      );
+    });
+
+    await Promise.all(inserts);
+
+    await db.query(
+      "UPDATE sincronizacion SET numero_sorteo = $1 WHERE id = 1",
+      [next]
     );
-  });
 
-  await Promise.all(inserts);
-
-  await db.query("UPDATE sincronizacion SET numero_sorteo = $1 WHERE id = 1", [
-    next,
-  ]);
-
-  log("Histórico agregado:", next);
+    log("Histórico agregado:", next);
+  } catch (error) {
+    if (err.code === "23505") {
+      // duplicado, ignorar
+    } else {
+      throw err;
+    }
+  }
 
   process.exit();
 }
